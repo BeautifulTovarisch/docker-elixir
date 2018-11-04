daceeba60e2e67f383554c56637475e86376d6a0
Anthony Re <anthonyjosephre@gmail.com>
2018-11-04 13:44:34 -0500

    migrated tests to use jest

--- client/Index/index.spec.js
+++ client/Index/index.spec.js
@@ -1,16 +1,14 @@
-// ( contrived ) Example function with failing test.
 // Edit add function to make test pass!
 
-import test   from 'ava';
 import React  from 'react';
 import render from 'react-test-renderer';
 
 const add = ( a, b ) => a + b;
 
-test( 'it should add two numbers', t => {
-    t.is( add( 1, 2 ), 3 );
+test( 'it should add two numbers', () => {
+    expect( add( 1, 2 ) ).toBe( 3 );
 });
 
-test( 'it should work with negative numbers', t => {
-    t.is( add( 1, -2 ), -1 );
+test( 'it should work with negative numbers', () => {
+    expect( add( 1, -2 ) ).toBe(-1 );
 });
