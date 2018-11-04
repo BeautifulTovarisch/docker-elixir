256ad96b2422b37c725c356e781781c3d607131f
Anthony Re <anthonyjosephre@gmail.com>
2018-11-04 13:12:44 -0500

    able to run dockerfile as non-root user with and without docker-compose + bash script

--- run-service.sh
+++ run-service.sh
@@ -4,5 +4,5 @@ if [ -z "$1" ]; then
    echo "Please provide a service name"
    exit 1
 else
-    USER=$(id -u):$(id -g) docker-compose up $1
+    USER_ID=$(id -u):$(id -g) docker-compose up $1
 fi
