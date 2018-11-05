94afc10ff76c44c6141a509a8cc2742979bfbcda
Anthony Re <anthonyjosephre@gmail.com>
2018-11-05 15:01:21 -0500

    blank elixir app created with mix

---
+++ server/lib/home.ex
@@ -0,0 +1,18 @@
+defmodule Home do
+  @moduledoc """
+  Documentation for Home.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Home.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
